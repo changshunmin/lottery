04a3ca0627b243300c1178eb6733a32a9e5f3072
changsm <changmsg@gmail.com>
2026-05-17 01:17:51 +0800

    win test

--- src/main/java/com/lottery/LotteryApplication.java
+++ src/main/java/com/lottery/LotteryApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LotteryApplication {
     public static void main(String[] args) {
         SpringApplication.run(LotteryApplication.class, args);
+        // TODO 打印logo
     }
 }
