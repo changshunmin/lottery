50cfecd3121ecfe761dac9cac96cd4bbe857e2b2
changsm <changmsg@sina.com>
2026-05-17 15:07:12 +0800

    打印测试

--- src/main/java/com/lottery/LotteryApplication.java
+++ src/main/java/com/lottery/LotteryApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LotteryApplication {
     public static void main(String[] args) {
         SpringApplication.run(LotteryApplication.class, args);
-        // TODO 打印logo
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%d×%d=%2d  ", j, i, i * j);
+            }
+            System.out.println();
+        }
     }
 }
